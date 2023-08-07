781becc7fce33b5a2c2d6c15134ddce38707600c
Coreychen4444 <coreychen1218@gmail.com>
2023-08-07 19:38:29 +0800

    second commit

--- main.go
+++ main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
+	"log"
+
 	"github.com/Coreychen4444/Users/model"
 	"github.com/Coreychen4444/Users/routers"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	"log"
 )
 
 func main() {
-	// 连接数据库
+	// 连接数据库(用户名和密码自己改)
 	dsn := "root:44447777@tcp(:3306)/users?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
